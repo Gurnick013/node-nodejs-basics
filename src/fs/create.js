8a1dc53250781c8c1955ff4c42bfa00e1da3922d
mikalai <mikalai.hurynovich@itechart-group.com>
2022-12-05 16:18:53 +0300

    feat: implemented creat func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { appendFile, existsSync } from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+    if (existsSync ('src/fs/files/fresh.txt')) {
+        console.log('File already exists');
+    } else {
+        appendFile('src/fs/files/fresh.txt', 'I am fresh and young', (err) => {
+            if (err) throw err;
+            console.log('File is created successfully.');
+        })
+    }
 };
 
 await create();
