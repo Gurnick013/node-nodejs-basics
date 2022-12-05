a65723c21c4118e006264d9b9b4c3c3def4c877e
mikalai <mikalai.hurynovich@itechart-group.com>
2022-12-05 17:37:47 +0300

    feat: implemented delete func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { unlinkSync, existsSync } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+  if (!existsSync ('src/fs/files')) {
+    console.log("There's no file");
+  } else {
+    try {
+      unlinkSync('src/fs/files/fileToRemove.txt');
+      console.log('File was successfully deleted');
+    } catch (err) {
+      console.log('Failed!');
+    }
+  }
 };
 
 await remove();
