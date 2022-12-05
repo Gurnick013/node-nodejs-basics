fe98180887eb1c2145d32e04ba52473e1b058973
mikalai <mikalai.hurynovich@itechart-group.com>
2022-12-06 01:09:59 +0300

    feat: finished

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,10 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process'
+
+const spawnChildProcess = async (...args) => {
+  spawn("node", ['src/cp/files/script.js', ...args], {
+    detached: true,
+    stdio: [process.stdin, process.stdout, process.stderr, "ipc"],
+  });
 };
 
 spawnChildProcess();
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { cpus } from 'node:os';
+import { Worker } from 'node:worker_threads';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const performCalculations = async () => {
-    // Write your code here
+  const CP = cpus()
+  let num = 10;
+  const workers = await Promise.allSettled(CP.map(() => {
+    return new Promise((res, rej) => {
+      const aloneWorker = new Worker(join(__dirname, 'worker.js'), {
+        workerData: num += 1
+      })
+      aloneWorker.on('message', (data) => res(data))
+      aloneWorker.on('error', (err) => rej(err))
+
+    })
+  }))
+
+  const results = workers.map((item) => {
+    const status = item.status === 'fulfilled' ? "resolve" : "reject";
+    const data = item.status === 'fulfilled' ? item.value : null;
+    return { status: status, data: data }
+  })
+  console.log(results);
 };
 
-await performCalculations();
+performCalculations();
