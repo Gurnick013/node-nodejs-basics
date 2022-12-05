f37f65a89c91cbd681ad257ce7f4ad39ce6a6595
mikalai <mikalai.hurynovich@itechart-group.com>
2022-12-05 17:28:05 +0300

    feat: implemented copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import { existsSync, cpSync } from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+  if (existsSync ('src/fs/files_copy')) {
+    console.log('Folder has already been created');
+  } else {
+    try {
+      cpSync('src/fs/files/', 'src/fs/files_copy/', { recursive: true })
+      console.log('Files was copied successfully!')
+    } catch (err) {
+      console.log('Failed!')
+    }
+  }
 };
 
 copy();
